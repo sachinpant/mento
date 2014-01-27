24a881ae3d16e9c23797525f0780dd30c995d00a
cx <cx12dev@gmail.com>
2014-01-27 21:47:40 +0100

    add basic error messages

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def invalid_call():
     return 'Mento has to be accessed using REST API calls.'
 
+@app.route('/manage/refresh')
+def refresh_library():
+    print 'Refreshing music library...'
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Invalid call.'
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return 'Invalid call.'
+
 if __name__ == '__main__':
     app.run(port=1337, host='0.0.0.0')
