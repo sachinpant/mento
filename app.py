26151d136491548471f042153c0a08269b846713
Thomas <thomas@fixforce.nl>
2014-02-07 15:06:26 +0100

    Test commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask import Flask, request, send_file, jsonify, Response
 from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-# set up the config and database
+# set up the config and database 
 app.config.from_object('config')
 db = SQLAlchemy(app)
 import models
