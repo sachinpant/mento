863156aa532a72b6e876a022dab4f98b98dde2fe
cx <cx12dev@gmail.com>
2014-02-10 10:15:17 +0100

    database setup works fine now

--- models.py
+++ models.py
@@ -20,7 +20,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from app import db
+from helper import db
 
 # privilege classes
 ROLE_USER = 0
